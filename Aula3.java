1123ab8fdc8c9df79a71b28d13f13a97c642833f
Rafael-Greed <luizjanuario200@gmail.com>
2026-09-02 09:44:16 -0300

    Delete Aula3.java

--- Aula3.java
+++
@@ -1,7 +0,0 @@
-public class Aula3 {
-    public static void main(String[] args) {
-        System.out.println("Hello World!!");
-        System.out.println("Minha idade é " + 18 + " anos");
-        System.out.println("resultado é: " + (3 + 5 <= 22));
-    }
-}
