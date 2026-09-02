4bc2610498f3f2de36b962d04b9bd155ca098ce5
Rafael-Greed <luizjanuario200@gmail.com>
2026-09-02 09:16:02 -0300

    Add files via upload

---
+++ Aula3.java
@@ -0,0 +1,7 @@
+public class Aula3 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+        System.out.println("Minha idade é " + 18 + " anos");
+        System.out.println("resultado é: " + (3 + 5 <= 22));
+    }
+}
