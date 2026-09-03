df06797fabbe84b634c1091e55320eb5f558275e
Rafael-Greed <luizjanuario200@gmail.com>
2026-09-03 12:30:38 -0300

    Delete Aula-3 directory

--- Aula-3/Exercicio1.java
+++
@@ -1,7 +0,0 @@
-public class Exercicio1 {
-    public static void main(String[] args) {
-        System.out.println("Nome: " + "Luiz Rafael Braga de Faria");
-        System.out.println("RA" + "06274271");
-        System.out.println("Curso: " + "SUPERIOR DE TECNOLOGIA EM ANÁLISE E DESENVOLVIMENTO DE SISTEMAS");
-    }
-}
