0a561b88776debd8cd1efd3b0208c58b4c562529
Luiz Rafael Braga de Faria <luizjanuario200@gmail.com>
2026-09-09 10:33:03 -0300

    Add Exercicio1.java to read and process an integer
    Implement main method to read an integer and print half if greater than 20.

---
+++ Aula-4/Exercicios/Exercicio1.java
@@ -0,0 +1,24 @@
+package Exercicios;
+import java.util.Scanner;
+
+public class At1 {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite o numero inteiro ");
+
+        int N1 = entrada.nextInt();
+
+        double D2 = N1/2;
+
+        if (N1 > 20 ){
+
+            System.out.println( "Seu Número é: " + D2);
+
+        }
+
+        entrada.close(); 
+
+    }
+}
